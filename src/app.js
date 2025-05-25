e05e874ae99900eb08005639901784b5ba5b04cf
AhmedYasein <ahmedyasein74@gmail.com>
2025-05-25 14:37:31 +0300

    add: Not found routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import express from "express";
-import userRoutes from './routes/userRoutes.js'; // 
-import tweetRoutes from "./routes/tweetRoutes.js"
+import userRoutes from './routes/userRoutes.js';
+import tweetRoutes from "./routes/tweetRoutes.js";
+import ResponseHandler from './utils/ResponseHandler.js';
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/user', userRoutes)
-app.use('/tweet', tweetRoutes)
+app.use('/user', userRoutes);
+app.use('/tweet', tweetRoutes);
 
-
-app.get("/", (req , res) => {
-  res.send("Hello from Express TypeScript app!");
+// 404 Not Found handler
+app.use((req, res, next) => {
+  return ResponseHandler.fail(res, 'Not Found', {}, 404);
 });
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  return ResponseHandler.error(res, err, 'Internal Server Error', 500);
+});
+
+
 
 export default app;
